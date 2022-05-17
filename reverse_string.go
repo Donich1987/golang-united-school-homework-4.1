d5f20a1f6b6b98e2f43b8f6f79510dd7be837310
Donich1987 <99492348+Donich1987@users.noreply.github.com>
2022-05-17 15:24:56 +0300

    Update reverse_string.go

--- reverse_string.go
+++ reverse_string.go
@@ -2,7 +2,7 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
-	bytes := []byte(input)
+	bytes := []rune(input)
 	for i := 0; i < len(bytes)-1; i++ {
 		for j := 0; j < len(bytes)-i-1; j++ {
 			bytes[j], bytes[j+1] = bytes[j+1], bytes[j]
