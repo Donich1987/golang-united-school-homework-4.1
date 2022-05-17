00a62951d6a150d14efcf2fb127336bd0df7a7b5
Donich1987 <Donich440542@mail.ru>
2022-05-17 15:03:42 +0300

    My best solution

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,12 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	bytes := []byte(input)
+	for i := 0; i < len(bytes)-1; i++ {
+		for j := 0; j < len(bytes)-i-1; j++ {
+			bytes[j], bytes[j+1] = bytes[j+1], bytes[j]
+		}
+	}
+	output = string(bytes)
 	return output
 }
